3c95e9887f47b2d390521b85aab0858a73b91207
manmao <man.mao@foxmail.com>
2024-01-03 15:24:36 +0800

    feature: globallock + select for update 全局锁和本地锁 防止分布式事务脏读和脏写

--- seata-account-service/src/main/java/com/scorpios/account/service/impl/AccountServiceImpl.java
+++ seata-account-service/src/main/java/com/scorpios/account/service/impl/AccountServiceImpl.java
@@ -2,12 +2,12 @@ package com.scorpios.account.service.impl;
 
 import com.scorpios.account.dao.AccountDao;
 import com.scorpios.account.service.AccountService;
+import io.seata.spring.annotation.GlobalLock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 账户业务实现类
@@ -23,12 +23,17 @@ public class AccountServiceImpl implements AccountService {
 
     /**
      * 扣减账户余额
+     * 全局锁 + select for update
+     * 1.加了 select for update语句会在执行 SQL 前检查全局锁是否存在，只有当全局锁完成之后，才能继续执行 SQL，这样就防止了脏读。
+     * 2.加了 select for update语句，则会在 update 前检查全局锁是否存在，只有当全局锁释放之后，其他事务才能开始执行 update 操作。防止了脏写。
+     *       select for update 也有个好处，就是可以重试。
      */
     @Override
+    @GlobalLock
     public void decrease(Long userId, BigDecimal money) {
 
         log.info("------->account-service中扣减账户余额开始");
-       /* try {
+       /*try {
             TimeUnit.SECONDS.sleep(20);
         } catch (InterruptedException e) {
             e.printStackTrace();
--- seata-storage-service/src/main/java/com/scorpios/storage/service/impl/StorageServiceImpl.java
+++ seata-storage-service/src/main/java/com/scorpios/storage/service/impl/StorageServiceImpl.java
@@ -2,7 +2,7 @@ package com.scorpios.storage.service.impl;
 
 import com.scorpios.storage.dao.StorageDao;
 import com.scorpios.storage.service.StorageService;
-import io.seata.spring.annotation.GlobalTransactional;
+import io.seata.spring.annotation.GlobalLock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +17,15 @@ public class StorageServiceImpl implements StorageService {
     @Resource
     private StorageDao storageDao;
 
+    /**
+     * 全局锁 + select for update
+     * 1.加了 select for update语句会在执行 SQL 前检查全局锁是否存在，只有当全局锁完成之后，才能继续执行 SQL，这样就防止了脏读。
+     * 2.加了 select for update语句，则会在 update 前检查全局锁是否存在，只有当全局锁释放之后，其他事务才能开始执行 update 操作。防止了脏写。
+     *   select for update 也有个好处，就是可以重试。
+     */
     // 扣减库存
     @Override
+    @GlobalLock
     public void decrease(Long productId, Integer count) {
         log.info("------->storage-service中扣减库存开始");
         storageDao.decrease(productId,count);
